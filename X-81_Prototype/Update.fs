f913101efc39087b9c324a3503b7d5d7d2c839b2
Ben Samuels <contact@bsamuels.net>
2014-05-10 10:53:42 -0700

    moving control update stuff into its own file

--- X-81_Prototype/Update.fs
+++ X-81_Prototype/Update.fs
@@ -180,51 +180,8 @@ module Update =
 
         newShipStates
 
-    let selectionTick gameState mouseState : ObjectId list =
-        if mouseState.Left.DragCompleted then
-            let isShipWithinArea area ship =
-                Monads.condition {
-                    do! Rectangle.containsVec area ship.Position
-                    do! ship.PlayerControlled
-                    return true
-                    }
-            match mouseState.Left.DraggedArea with
-                |Some(area) -> 
-                    let selectedShips = gameState.Ships |> List.filter (isShipWithinArea area)
-                    selectedShips |> List.map (fun s -> s.Id)
-                | None -> 
-                    failwith "that shouldnt happen"; []
-        elif mouseState.Left.ClickCompleted then
-            let isCursorWithinArea cursor ship =
-                Monads.condition {
-                    do! Rectangle.containsVec ship.AABB cursor
-                    do! ship.PlayerControlled
-                    return true                
-                    }
-            let selectedShip = gameState.Ships |> List.tryFind (isCursorWithinArea mouseState.WorldPosition)
-            match selectedShip with
-            | Some(ship) -> [ship.Id]
-            | None -> []
-        else
-            gameState.SelectedShips
 
 
-    let movementTarSelectTick gameState mouseState =
-        if mouseState.Right.ClickCompleted then
-            gameState.Ships |> List.map (
-                fun ship ->
-                    if List.exists ship.Id.Equals gameState.SelectedShips then
-                        {ship with AiMovementState = AiMovementState.MovingToPoint(mouseState.WorldPosition)}
-                    else
-                        ship
-            )
-        else
-            gameState.Ships
 
 
-    let zoomTick gameState mouseState =
-        if mouseState.ScrollWheelDelta <> 0 then
-            modifyViewScale gameState.GameView ((float mouseState.ScrollWheelDelta) * -0.2<m/px>)
-        else
-            gameState.GameView
 
