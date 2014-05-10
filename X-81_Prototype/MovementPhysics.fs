688ded308e666f6abdc84d58c1e4a6acfa956986
Ben Samuels <contact@bsamuels.net>
2014-05-10 11:08:37 -0700

    splitting movement physics into its own module

---
+++ X-81_Prototype/MovementPhysics.fs
@@ -0,0 +1,107 @@
+﻿namespace global
+
+module MovementPhysics =
+    open System;
+    open SFML.Window;
+
+    let private calcVelCeil distToDest (curVel:float<m/s>) shipAttribs =
+        if curVel*1.0<s> > distToDest then
+            ()
+        
+
+        let ceilCandid = (((curVel * (shipAttribs.AccelerationFactor - 1.0)) + shipAttribs.MaxVel)/shipAttribs.AccelerationFactor)
+        if ceilCandid < shipAttribs.VelBoost then
+            shipAttribs.VelBoost
+        else
+            ceilCandid
+
+    let getLinearVelCeil ship dest : Vec2<m/s> =
+        let velocity = ship.Velocity
+        let rotation = ship.Rotation
+        let shipAttribs = ship.Attribs
+        let distToDest = Vec2.distance ship.Position dest
+
+        let forwardUnit = Vec2.getFromAngle (rotation) 1.0
+        let sidewaysUnit = Vec2.getFromAngle (rotation-1.55<rad>) 1.0
+        let sidewaysProjection = velocity.X*sidewaysUnit.X + velocity.Y*sidewaysUnit.Y
+        let forwardProjection = velocity.X*forwardUnit.X + velocity.Y*forwardUnit.Y
+
+        let forwardAccel = - calcVelCeil distToDest sidewaysProjection shipAttribs
+        let reverseAccel = 0.0<m/s>
+        let portAccel = 0.0<m/s>
+        let starboardAccel = 0.0<m/s>
+
+        let sumForward = forwardAccel + reverseAccel
+        let sumSideways = starboardAccel + portAccel
+        
+        let x = forwardUnit.X * sumSideways - sumForward * forwardUnit.Y
+        let y = forwardUnit.Y * sumSideways + sumForward * forwardUnit.X
+
+        {X=x; Y=y}
+
+    let private getShipInputRotAccel keyboardState =
+        let qState = (Control.getKeyState keyboardState Keyboard.Key.Q).KeyState
+        let eState = (Control.getKeyState keyboardState Keyboard.Key.E).KeyState
+        let rotAccelCCW = 
+            match qState with 
+            | KeyState.Pressed -> -Consts.rotAccel
+            | _ -> 0.0<rad/s^2>
+        let rotAccelCW = 
+            match eState with 
+            | KeyState.Pressed -> Consts.rotAccel
+            | _ -> 0.0<rad/s^2>
+        let accelSum = rotAccelCCW + rotAccelCW
+        accelSum
+
+    let private applyFriction (vel:float<'u>) (accel:float<'v>) (decelQty:float<'v>) =
+        if accel = 0.0<_> then
+            let sign = vel / abs(vel)
+            if sign <> sign then //this returns false if the float is NaN
+                0.0<_>
+            else
+                if abs(vel * (1.0/60.0)) < decelQty * 1.0<s> then
+                    -sign * abs(vel) * 1.0</s>
+                else
+                    -sign * decelQty
+        else
+            accel
+
+    let applyLinFriction vel accel : Vec2<m/s^2>=
+        let decelConst = Consts.linAccel * 10.0
+        let frictionedX = applyFriction vel.X accel.X decelConst
+        let frictionedY = applyFriction vel.Y accel.Y decelConst
+        
+        {X=frictionedX; Y=frictionedY}
+
+    let private clampAngle (ang:float<rad>) =
+        let rec clamp ang =
+            if ang >= Math.PI * 1.0<rad> then
+                clamp (ang - (Math.PI * 2.0<rad>))
+            elif ang < Math.PI * -1.0<rad> then
+                clamp (ang + (Math.PI * 2.0<rad>))
+            else ang
+        clamp ang
+
+    let calcRotVelFromTargAngle (prevShipState:ShipState) target =        
+        let posDiff = target -. prevShipState.Position
+        let targetAngle = Math.Atan2(float posDiff.Y, float posDiff.X) * 1.0<rad> + Math.PI / 2.0 * 1.0<rad>
+
+        let angleDiff = clampAngle (targetAngle - prevShipState.Rotation)
+        let angleDist = abs(angleDiff)
+        let slowingRadius = 0.001<rad>
+
+        let clampAngVel mvel vel =
+            if vel > mvel then
+                mvel
+            elif abs(vel) > mvel then
+                -mvel
+            else vel
+
+        let vel = 
+            if angleDist < slowingRadius then
+                let desiredVel = (angleDiff * prevShipState.Attribs.AngVelScale * (angleDist / slowingRadius))
+                clampAngVel prevShipState.Attribs.MaxAngVel desiredVel
+            else
+                let desiredVel = (angleDiff * prevShipState.Attribs.AngVelScale)
+                clampAngVel prevShipState.Attribs.MaxAngVel desiredVel
+        vel
--- X-81_Prototype/Update.fs
+++ X-81_Prototype/Update.fs
@@ -4,164 +4,48 @@ module Update =
     open System;
     open SFML.Window;
 
-    let private calcVelCeil (curVel:float<m/s>) shipAttribs =
-        let ceilCandid = ((curVel * (shipAttribs.SlowingFactor - 1.0)) + shipAttribs.MaxVel)/shipAttribs.SlowingFactor
-        if ceilCandid < shipAttribs.VelBoost  then
-            shipAttribs.VelBoost
-        else
-            ceilCandid
-
-    let private getLinearVelCeil ship dest : Vec2<m/s> =
-        let velocity = ship.Velocity
-        let rotation = ship.Rotation
-        let shipAttribs = ship.Attribs
-        (*
-        let wState = (Control.getKeyState keyboardState Keyboard.Key.W).KeyState
-        let aState = (Control.getKeyState keyboardState Keyboard.Key.A).KeyState
-        let sState = (Control.getKeyState keyboardState Keyboard.Key.S).KeyState
-        let dState = (Control.getKeyState keyboardState Keyboard.Key.D).KeyState
-        *)
-        let forwardUnit = Vec2.getFromAngle (rotation) 1.0
-        let sidewaysUnit = Vec2.getFromAngle (rotation-1.55<rad>) 1.0
-        let sidewaysProjection = velocity.X*sidewaysUnit.X + velocity.Y*sidewaysUnit.Y
-        let forwardProjection = velocity.X*forwardUnit.X + velocity.Y*forwardUnit.Y
-        (*
-        let forwardAccel = 
-            match wState with 
-            | KeyState.Pressed -> - calcVelCeil sidewaysProjection shipAttribs
-            | _ -> 0.0<m/s>
-        let reverseAccel = 
-            match sState with 
-            | KeyState.Pressed -> calcVelCeil -sidewaysProjection shipAttribs
-            | _ -> 0.0<m/s>
-        let portAccel = 
-            match aState with 
-            | KeyState.Pressed -> -calcVelCeil -forwardProjection shipAttribs
-            | _ -> 0.0<m/s>
-        let starboardAccel = 
-            match dState with 
-            | KeyState.Pressed -> calcVelCeil forwardProjection shipAttribs
-            | _ -> 0.0<m/s>
-        *)
-        let forwardAccel = - calcVelCeil sidewaysProjection shipAttribs
-        let reverseAccel = 0.0<m/s>
-        let portAccel = 0.0<m/s>
-        let starboardAccel = 0.0<m/s>
-
-        let sumForward = forwardAccel+reverseAccel
-        let sumSideways = starboardAccel + portAccel
-        
-        //let x = forwardUnit.X * sumSideways - sumForward * forwardUnit.Y
-        //let y = forwardUnit.Y * sumSideways + sumForward * forwardUnit.X
-
-        {X=sumSideways; Y=sumForward}
-
-    let private getShipInputRotAccel keyboardState =
-        let qState = (Control.getKeyState keyboardState Keyboard.Key.Q).KeyState
-        let eState = (Control.getKeyState keyboardState Keyboard.Key.E).KeyState
-        let rotAccelCCW = 
-            match qState with 
-            | KeyState.Pressed -> -Consts.rotAccel
-            | _ -> 0.0<rad/s^2>
-        let rotAccelCW = 
-            match eState with 
-            | KeyState.Pressed -> Consts.rotAccel
-            | _ -> 0.0<rad/s^2>
-        let accelSum = rotAccelCCW + rotAccelCW
-        accelSum
-
-    let private applyFriction (vel:float<'u>) (accel:float<'v>) (decelQty:float<'v>) =
-        if accel = 0.0<_> then
-            let sign = vel / abs(vel)
-            if sign <> sign then //this returns false if the float is NaN
-                0.0<_>
-            else
-                if abs(vel * (1.0/60.0)) < decelQty * 1.0<s> then
-                    -sign * abs(vel) * 1.0</s>
-                else
-                    -sign * decelQty
-        else
-            accel
-
-    let private applyLinFriction vel accel : Vec2<m/s^2>=
-        let decelConst = Consts.linAccel * 2.0
-        let frictionedX = applyFriction vel.X accel.X decelConst
-        let frictionedY = applyFriction vel.Y accel.Y decelConst
-        
-        {X=frictionedX; Y=frictionedY}
-
-    let private clampAngle (ang:float<rad>) =
-        let rec clamp ang =
-            if ang >= Math.PI * 1.0<rad> then
-                clamp (ang - (Math.PI * 2.0<rad>))
-            elif ang < Math.PI * -1.0<rad> then
-                clamp (ang + (Math.PI * 2.0<rad>))
-            else ang
-        clamp ang
-
-    let private calcRotVelFromMousepos (prevShipState:ShipState) target =        
-        let posDiff = target - prevShipState.Position
-        let targetAngle = Math.Atan2(float posDiff.Y, float posDiff.X) * 1.0<rad> + Math.PI / 2.0 * 1.0<rad>
-
-        let angleDiff = clampAngle (targetAngle - prevShipState.Rotation)
-        let angleDist = abs(angleDiff)
-        let slowingRadius = 0.001<rad>
-
-        let clampAngVel mvel vel =
-            if vel > mvel then
-                mvel
-            elif abs(vel) > mvel then
-                -mvel
-            else vel
-
-        let vel = 
-            if angleDist < slowingRadius then
-                let desiredVel = (angleDiff * prevShipState.Attribs.AngVelScale * (angleDist / slowingRadius))
-                clampAngVel prevShipState.Attribs.MaxAngVel desiredVel
-            else
-                let desiredVel = (angleDiff * prevShipState.Attribs.AngVelScale)
-                clampAngVel prevShipState.Attribs.MaxAngVel desiredVel
-        vel
-
-
     let private rotationTick (shipState:ShipState) dest=
-        let rotVel =
-            //if mouseState.RightPressed then
-            calcRotVelFromMousepos shipState dest
-            (*else
-                let rawRotAccel = getShipInputRotAccel keyboardState
-                let accel = applyFriction prevShipState.RotVelocity rawRotAccel Consts.rotAccel
-                prevShipState.RotVelocity + accel * (1.0<s>/60.0)*)
+        let rotVel = MovementPhysics.calcRotVelFromTargAngle shipState dest
 
         let newShipRot = shipState.Rotation + rotVel * (1.0<s>/60.0)
         (newShipRot, rotVel)
 
 
-    let private linearTick (shipState:ShipState) dest =
-        let newVelCeil =  getLinearVelCeil shipState dest
+    let private linearTick (shipState:ShipState) newVel =
+        let newShipSpeed : Vec2<m/s> = ((shipState.Velocity *. (20.0 - 1.0)) +. newVel) /. 20.0
+        let newShipPos = shipState.Position +. newShipSpeed *. (1.0<s>/60.0)
 
-        let newShipSpeed = ((shipState.Velocity * (20.0 - 1.0)) + newVelCeil)/20.0
-        let newShipPos = shipState.Position + shipState.Velocity * (1.0<s>/60.0)
-        let shipAccel = (newShipSpeed - shipState.Velocity) * (1.0/60.0<s>)
-        let newAABBpos = shipState.Attribs.AABBShape + newShipPos
+        let shipAccel = (newShipSpeed -. shipState.Velocity) *. (1.0/60.0<s>)
+        let newAABBpos = shipState.Attribs.AABBShape +. newShipPos
         (newShipPos, newShipSpeed, shipAccel, newAABBpos)
 
     let private moveToTarget (shipState:ShipState) dest =
         let (newShipRot, newShipRotVel) = rotationTick shipState dest
-        let (newShipPos, newShipSpeed, newAccel, newAABB) = linearTick shipState dest
+        let newVel = MovementPhysics.getLinearVelCeil shipState (dest)
+        let (newShipPos, newShipSpeed, newAccel, newAABB) = linearTick shipState (newVel)
 
         Draw.addWorldDebugLine [newShipPos; dest]
-        let l = (Vec2<m>.getFromAngle (newShipRot - Math.PI / 2.0 * 1.0<rad>) 50.0<m>) + newShipPos
+        let l = (Vec2<m>.getFromAngle (newShipRot - Math.PI / 2.0 * 1.0<rad>) 50.0<m>) +. newShipPos
         Draw.addWorldDebugLine [l;newShipPos]
 
         {shipState with Position=newShipPos; Velocity=newShipSpeed; Rotation=newShipRot; RotVelocity=newShipRotVel; Acceleration=newAccel; AABB=newAABB}
 
+    let private tickIdleShip ship =
+        let friction = MovementPhysics.applyLinFriction ship.Velocity ship.Acceleration
+        let newShipSpeed = ship.Velocity +. friction *. 1.0<s>
+
+        let newShipPos = ship.Position +. newShipSpeed *. (1.0<s>/60.0)
+
+        let shipAccel = (newShipSpeed -. ship.Velocity) *. (1.0/60.0<s>)
+        let newAABBpos = ship.Attribs.AABBShape +. newShipPos
+        
+        {ship with Position=newShipPos; Velocity=newShipSpeed; Acceleration=shipAccel; AABB=newAABBpos}
 
     let private shipAiTick ship =
         let postMovementTick = 
             match ship.AiMovementState with
-                | AiMovementState.Idle -> ship
-                | AiMovementState.MovingToPoint(pt) -> moveToTarget ship pt
+                | AiMovementState.Idle -> tickIdleShip ship
+                | AiMovementState.MovingToPoint(pt) -> moveToTarget ship pt.Dest
                 | _ -> failwith "not supported"
         let postCombatTick =
             match ship.AiCombatState with
@@ -172,16 +56,21 @@ module Update =
 
 
     let allShipsTick curShips keyboardState mouseState=
-        let tick ship =
+        let aiTick ship =
             match ship.PlayerControlled with
             | true -> shipAiTick ship
             | false -> ship
-        let newShipStates = curShips |> List.map tick
+        let postAiTick = curShips |> List.map aiTick
 
-        newShipStates
+        let postAiCleanup = postAiTick |> List.map ( fun ship ->
+            match ship.AiMovementState with
+            | AiMovementState.MovingToPoint(dest) -> 
+                if Vec2.distance dest.Dest ship.Position < 5.0<m> then
+                    {ship with AiMovementState = AiMovementState.Idle}
+                else
+                    ship
+            | _ -> ship
+        )
 
-
-
-
-
-
+        //add stop-moving-to-point handler here
+        postAiCleanup
